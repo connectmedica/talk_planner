193fc67f82cb4f21a5332ac0589d4381054876af
Kamil Kucharski <kamil.kucharski88@gmail.com>
2012-01-04 21:11:52 +0100

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TalkPlanner::Application.config.secret_token = 'aa7aa3d2dcfb117a6eb144e884c3a731a723e5066d3eea9cd12194c318ed1de982f69c52fc958b95923ccbba231bea2db50f27aea5ed88f8137e3e64c00c431b'
